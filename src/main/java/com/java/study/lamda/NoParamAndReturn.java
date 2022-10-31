b9f84b641a806d20390ca2b760aedd531f0ce0cb
nataekoon <smathj@naver.com>
2022-11-01 00:12:31 +0900

    2022.10.31 - 람다2, 함수형 인터페이스

---
+++ src/main/java/com/java/study/lamda/NoParamAndReturn.java
@@ -0,0 +1,25 @@
+package com.java.study.lamda;
+
+
+import java.util.Random;
+
+interface Generator {
+    int rand(); // 매개변수 없는 메소드
+}
+
+public class NoParamAndReturn {
+    public static void main(String[] args) {
+        Generator gen = () -> {
+            Random rand = new Random();
+            return rand.nextInt(50); // 50 미만
+        };
+
+        int max = 0;
+        for (int i = 0; i < 100; i++) {
+            if (gen.rand() >= max) {
+                max = gen.rand();
+            }
+        }
+        System.out.println(max);
+    }
+}
--- src/main/java/com/java/study/lamda/TwoParamAndReturn.java
+++ src/main/java/com/java/study/lamda/TwoParamAndReturn.java
@@ -1,19 +1,19 @@
-package com.java.study.lamda;
-
-
-interface Calculate {
-    int cal(int a, int b);
-}
-
-public class TwoParamAndReturn {
-    public static void main(String[] args) {
-        Calculate c;
-
-        c = (a,b) -> { return (a + b); };
-        System.out.println(c.cal(4,3));
-
-        c = (a,b) -> a + b; // return 생략
-        System.out.println(c.cal(4,3));
-
-    }
-}
+//package com.java.study.lamda;
+//
+//
+//interface Calculate {
+//    int cal(int a, int b);
+//}
+//
+//public class TwoParamAndReturn {
+//    public static void main(String[] args) {
+//        Calculate c;
+//
+//        c = (a,b) -> { return (a + b); };
+//        System.out.println(c.cal(4,3));
+//
+//        c = (a,b) -> a + b; // return 생략
+//        System.out.println(c.cal(4,3));
+//
+//    }
+//}
