a97a4387eaa9c7eb0199f50b16295569cdea3f4b
nataekoon <smathj@naver.com>
2022-11-08 23:29:53 +0900

    2022.11.07 - thread first day

---
+++ src/main/java/com/java/study/thread/thread01/CurrentThreadName.java
@@ -0,0 +1,9 @@
+package com.java.study.thread.thread01;
+
+public class CurrentThreadName {
+    public static void main(String[] args) {
+        Thread ct = Thread.currentThread();
+        String name = ct.getName();
+        System.out.println(name);
+    }
+}
