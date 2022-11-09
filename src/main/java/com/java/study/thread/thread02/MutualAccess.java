9c2de80b84e49087734931f8bc95dc988c7c4275
nataekoon <smathj@naver.com>
2022-11-09 23:24:04 +0900

    2022.11.07 - thread end

---
+++ src/main/java/com/java/study/thread/thread02/MutualAccess.java
@@ -0,0 +1,46 @@
+package com.java.study.thread.thread02;
+
+
+class Counter {
+    int count = 0;  // 두 쓰레드에 의해 공유 되는 변수
+
+    public void increment() {
+        count++;    // 첫번째 쓰레드에 의해 실행되는 메서드
+    }
+
+    public void decrement() {
+        count--;    // 두번째 쓰레드에 의해 실행되는 메서드
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class MutualAccess {
+
+    public static Counter cnt = new Counter();
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task1 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                cnt.increment();    // 값 1 증가
+                System.out.println("🌿 task1: " + cnt.getCount() + "[" + i + "]");
+            }
+        };
+        Runnable task2 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                cnt.decrement();    // 값 1 감소
+                System.out.println("🔥 task2: " + cnt.getCount() + "[" + i + "]");
+            }
+        };
+
+        Thread t1 = new Thread(task1);
+        Thread t2 = new Thread(task2);
+        t1.start();
+        t2.start();
+        t1.join();  // t1이 참조하는 쓰레드의 종료를 기다린다, 즉 main 쓰레드 t1 쓰레드의 종료를 기다린다 ( await 처럼 )
+        t2.join();  // t1이 참조하는 쓰레드의 종료를 기다린다, 즉 main 쓰레드 t2 쓰레드의 종료를 기다린다 ( await 처럼 )
+        System.out.println("cnt = " + cnt.getCount());  // 항상 0이 아니다
+    }
+}
---
+++ src/main/java/com/java/study/thread/thread03/MutualAccessReentrantLock.java
@@ -0,0 +1,62 @@
+package com.java.study.thread.thread03;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.ReentrantLock;
+
+class Counter {
+    int count = 0;
+    ReentrantLock criticObj = new ReentrantLock();
+
+    public void increment() {
+        criticObj.lock();
+        try {
+            count++;
+        } finally {
+            criticObj.unlock();
+        }
+    }
+
+    public void decrement() {
+        criticObj.lock();
+        try {
+            count--;
+        } finally {
+            criticObj.unlock();
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+public class MutualAccessReentrantLock {
+    public static Counter cnt = new Counter();
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable task1 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                cnt.increment();
+            }
+        };
+
+        Runnable task2 = () -> {
+            for (int i = 0; i < 1000; i++) {
+                cnt.decrement();
+            }
+        };
+
+        ExecutorService exr = Executors.newFixedThreadPool(2);
+        exr.submit(task1);
+        exr.submit(task2);
+
+        exr.shutdown();
+        exr.awaitTermination(100, TimeUnit.SECONDS);    // 쓰레드 풀에 전달되 작업이 끝나기를 100초간 기다린다
+        System.out.println(cnt.getCount());
+        
+    }
+
+}
